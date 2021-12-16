dfdefc03e4d015eee0b19386961459c79727ab3b
Chelsea <chelseatwan@gmail.com>
2021-12-16 14:47:12 -0500

    testing

--- python code.py
+++ python code.py
@@ -4,8 +4,6 @@ Created on Thu Dec 16 13:32:49 2021
 
 @author: chels
 """
-pip install streamlit
-streamlit hello
 
 import streamlit as st
 import pandas as pd
@@ -22,5 +20,7 @@ df_inpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_
 df_outpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
 
 st.dataframe(df_hospital)
+
 st.dataframe(df_inpatient)
+
 st.dataframe(df_outpatient)
