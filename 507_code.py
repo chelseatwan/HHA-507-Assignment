8d085d57ae850e054fd1b6a7499e3565a3eceeb6
Chelsea <chelseatwan@gmail.com>
2021-12-17 00:06:54 -0500

    testing second update

--- 507_code.py
+++ 507_code.py
@@ -8,10 +8,11 @@ Created on Thu Dec 16 13:32:49 2021
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.title('is it working')
-
-st.text('i hope it is working')
+#Title of app
+st.title('HHA 507 Final Assignment')
 
 df_hospital = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
 
@@ -19,8 +20,104 @@ df_inpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_
 
 df_outpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
 
+#Introduce first dataset - hospitals
+st.header('Hospital Data Preview')
 st.dataframe(df_hospital)
 
-st.dataframe(df_inpatient)
+#Look at only NY hospitals
+hospitals_ny = df_hospital[df_hospital['state'] == 'NY']
 
-st.dataframe(df_outpatient)
+#Look at columns and counts
+hospitals_ny.info()
+
+#Clean out data
+hospitals_ny.dropna()
+hospitals_ny_rating = hospitals_ny[-hospitals_ny['hospital_overall_rating'].isnull()]
+hospitals_ny_rating['hospital_overall_rating'].isna().sum()
+hospitals_ny_rating.info()
+
+#Pivot table looking at effectiveness of care in NY hospitals
+hospitals_ny_rating['hospital_overall_rating'] = hospitals_ny_rating['hospital_overall_rating'].astype('int')
+hospitals_ny_rating_pivot = hospitals_ny_rating.pivot_table(index=['state', 'city'],values=['hospital_overall_rating'])
+hospitals_ny_rating_pivot
+
+#Introduce second dataset - inpatient
+st.header('Inpatient Data Preview for NY')
+
+#Clean dataset
+df_inpatient.dropna()
+
+#Look at only NY hospitals
+inpatient_ny = df_inpatient[df_inpatient['provider_state'] == 'NY']
+st.dataframe(inpatient_ny)
+
+#Looking at common discharges and DRGs for Stony Brook
+common_discharges = inpatient_ny[inpatient_ny['provider_name'] == 'UNIVERSITY HOSPITAL ( STONY BROOK )']
+common_discharges = common_discharges.groupby('drg_definition')['total_discharges'].sum().reset_index()
+top10_inpatient = common_discharges.head(10)
+bottom10_inpatient = common_discharges.tail(10)
+
+st.header('DRGs')
+st.dataframe(common_discharges)
+
+col1, col2 = st.beta_columns(2)
+col1.header('Top 10 common DRGs')
+col1.dataframe(top10_inpatient)
+col2.header('Bottom 10 common DRGs')
+col2.dataframe(bottom10_inpatient)
+
+#Looking at DRGs and average costs for Stony Brook
+cost_discharges = inpatient_ny[inpatient_ny['provider_name'] == 'UNIVERSITY HOSPITAL ( STONY BROOK )']
+cost_discharges = cost_discharges.groupby('drg_definition')['average_total_payments'].sum().reset_index()
+top10_costs_inpatient = cost_discharges.head(10)
+bottom10_costs_inpatient = cost_discharges.tail(10)
+
+st.header('DRGs costs')
+st.dataframe(cost_discharges)
+
+col1, col2 = st.beta_columns(2)
+col1.header('Most expensive DRGs')
+col1.dataframe(top10_costs_inpatient)
+col2.header('Cheapest DRGs')
+col2.dataframe(bottom10_costs_inpatient)
+
+
+#Introduce third dataset - outpatient
+st.header('Outpatient Data Preview for NY')
+
+#Clean dataset
+df_outpatient.dropna()
+
+#Look at only NY hospitals
+outpatient_ny = df_outpatient[df_outpatient['provider_state'] == 'NY']
+st.dataframe(outpatient_ny)
+
+#Looking at common discharges and DRGs for Stony Brook
+common_discharges_outpatient = outpatient_ny[outpatient_ny['provider_name'] == 'University Hospital ( Stony Brook )']
+common_discharges_outpatient = common_discharges_outpatient.groupby('apc')['outpatient_services'].sum().reset_index()
+top10_discharges_outpatient = common_discharges_outpatient.head(10)
+bottom10_discharges_outpatient = common_discharges_outpatient.tail(10)
+
+st.header('DRGs')
+st.dataframe(common_discharges_outpatient)
+
+col1, col2 = st.beta_columns(2)
+col1.header('Top 10 common DRGs at Stony Brook Hospital')
+col1.dataframe(top10_discharges_outpatient)
+col2.header('Bottom 10 common DRGs at Stony Brook Hospital')
+col2.dataframe(bottom10_discharges_outpatient)
+
+#Looking at DRGs and average costs for Stony Brook
+cost_discharges_outpatient = outpatient_ny[outpatient_ny['provider_name'] == 'University Hospital ( Stony Brook )']
+cost_discharges_outpatient = cost_discharges_outpatient.groupby('apc')['average_total_payments'].sum().reset_index()
+top10_costs_outpatient = cost_discharges_outpatient.head(10)
+bottom10_costs_outpatient = cost_discharges_outpatient.tail(10)
+
+st.header('DRGs costs')
+st.dataframe(cost_discharges_outpatient)
+
+col1, col2 = st.beta_columns(2)
+col1.header('Most expensive outpatient DRGs at Stony Brook Hospital')
+col1.dataframe(top10_costs_outpatient)
+col2.header('Cheapest outpatient DRGs at Stony Brook Hospital')
+col2.dataframe(bottom10_costs_outpatient)
